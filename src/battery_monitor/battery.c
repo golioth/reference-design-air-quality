f945def15c20571509b85166cd5ba7f6a264e055
Chris Wilson <chris@cgnd.dev>
2023-08-29 22:55:28 -0700

    Don't log battery JSON buffer
    Signed-off-by: Chris Wilson <chris@cgnd.dev>

--- src/battery_monitor/battery.c
+++ src/battery_monitor/battery.c
@@ -320,7 +320,6 @@ int stream_battery_data(struct battery_data *batt_data)
 	snprintk(json_buf, sizeof(json_buf), JSON_FMT, batt_data->battery_voltage_mv / 1000,
 		 batt_data->battery_voltage_mv % 1000, batt_data->battery_level_pptt / 100,
 		 batt_data->battery_level_pptt % 100);
-	LOG_DBG("%s", json_buf);
 
 	err = golioth_stream_push(client, stream_endpoint, GOLIOTH_CONTENT_FORMAT_APP_JSON,
 				  json_buf, strlen(json_buf));
